8828941fe2d57d2c79eaeb28382ebc2d02497c44
Adam Brenneis <adam.brenneis@sas.com>
2026-02-11 18:18:55 +0000

    1.79.1

--- Package.swift
+++ Package.swift
@@ -8,11 +8,14 @@ let package = Package(
             name: "SASCollector",
             targets: ["SASCollector"]),
     ],
+    dependencies: [
+        // No dependencies
+    ],
     targets: [
         .binaryTarget(
             name: "SASCollector",
-            url: "https://sdk.ci360.sas.com/ios/1.79.0/SASCollector-1.79.0.zip",
-            checksum: "f6199899ab00ff1d95509d1a7ca35a1ef4861d071b8d1c6090a4f6ebbc27e20d"
+            url: "https://sdk.ci360.sas.com/ios/1.79.1/SASCollector-1.79.1.zip",
+            checksum: "78ae8bfa263d55fbfcc70e6297e883fa501c96d6e7b4867c1ddbdcca787b92aa"
         )
     ]
 )
